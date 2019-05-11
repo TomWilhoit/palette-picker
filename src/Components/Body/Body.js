1bc6c4051078012761ab6006ed8c51244f9a0760
Mason France <masonrf1@gmail.com>
2019-05-10 22:38:54 -0600

    Sets up grid layout of main page

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { PalettePicker } from '../../Containers/PalettePicker/PalettePicker'
+import { Projects } from '../../Containers/Projects/Projects'
+import { Control} from '../../Containers/Control/Control'
 
 export const Body = () => {
   return(
-    <div>
-      <div><PalettePicker /></div>
-      <div></div>
-      <div></div>
+    <div className='body'>
+      <div className='palette-picker'><PalettePicker /></div>
+      <div className='projects'><Projects /></div>
+      <div className='control'><Control /></div>
     </div>
   )
 }
---
+++ src/Containers/Projects/Projects.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export class Projects extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  // addProject = () => {}
+
+  // function to gather project names
+  // thinking we make Project component for each one
+  // may make delete easier
+
+  render() {
+    return(
+      <div>
+        <div>Add Project</div>
+      </div>
+    )
+  }
+}
