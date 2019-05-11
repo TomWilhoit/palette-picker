88078fcdab6fe3c303830d1519da09ad397f6800
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-05-11 15:03:21 -0600

    Maps state to props in Projects container and displays appropriate project

--- src/Containers/Project/Project.js
+++ src/Containers/Project/Project.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 
 export class Project extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
-
     }
   }
 
 
   render() {
-    
+    console.log(this.props)
     return(
       <div className='project'>
-        <h3>Project Name</h3>
+        <h3>{this.props.name}</h3>
       </div>
     )
   }
--- src/Containers/Projects/Projects.js
+++ src/Containers/Projects/Projects.js
@@ -1,27 +1,46 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { addProjects } from "../../Actions/index";
 import { Project } from '../Project/Project'
+import PropTypes from "prop-types";
 
 export class Projects extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
-
+      currentProject: null
     }
   }
 
-  // addProject = () => {}
-
-  // function to gather project names
-  // thinking we make Project component for each one
-  // may make delete easier
+ 
 
   render() {
-    return(
-      <div className='projects'>
-        <div className='add-project'>Add Project</div>
-        <Project />
-        <Project />
-      </div>
-    )
+    const displayProjects = this.props.projects.map((project, index) => (
+      <Project key={index} {...project} />
+    ));
+    if (this.props.projects.length === 0) {
+      return <div>Loading</div>;
+    } else {
+      return <div>{displayProjects}</div>;
+    }
   }
 }
+
+Projects.propTypes = {
+  projects: PropTypes.array,
+  palettes: PropTypes.array
+};
+
+export const mapStateToProps = state => ({
+  projects: state.projects,
+  palettes: state.palettes
+});
+
+export const mapDispatchToProps = dispatch => ({
+  addProjects: project => dispatch(addProjects(project))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Projects);
