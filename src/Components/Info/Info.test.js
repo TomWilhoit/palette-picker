bb88bab29bf10b758f7cd39508361c14b1d7da75
Mason France <masonrf1@gmail.com>
2019-10-25 23:20:34 -0600

    Add base of tests for Info and api

--- src/Components/Info/Info.test.js
+++ src/Components/Info/Info.test.js
@@ -1,68 +1,113 @@
 import React from "react";
 import { Info} from "./Info";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 describe("Info", () => {
   let wrapper;
+  
   beforeEach(() => {
-    wrapper = shallow(<Info />);
+    wrapper = mount(<Info hideInfo={jest.fn()} />);
   })
 
-  it("should match the snapshot with all data passed in", () => {
-    expect(wrapper).toMatchSnapshot();
+  describe("on load", () => {
+    it("should match the snapshot", () => {
+      expect(wrapper).toMatchSnapshot();
+    })
   })
 
   describe("handleClick", () => {
     it("should activate when close-info-btn is pressed", () => {
-
+      const mockEvent = { target: { className: "JIM" }};
+      wrapper.instance().handleClick = jest.fn();
+      wrapper.find(".close-info-btn").first().simulate("click", mockEvent);
+      expect(wrapper.instance().handleClick).toHaveBeenCalled();
     })
 
     it("should activate when an instruction-step is pressed", () => {
-
+      const mockEvent = { target: { className: "JIM" }};
+      wrapper.instance().handleClick = jest.fn();
+      wrapper.find(".instruction-step").first().simulate("click", mockEvent);
+      expect(wrapper.instance().handleClick).toHaveBeenCalled();
     })
 
-    it("should activate when a detail-box is pressed", () => {
-
+    it.skip("should activate when a instruction-box is pressed", () => {
+      const mockEvent = { target: { className: "JIM" }};
+      wrapper.instance().handleClick = jest.fn();
+      wrapper.find(".instruction-box").first().simulate("click", mockEvent);
+      expect(wrapper.instance().handleClick).toHaveBeenCalled();
     })
 
     it("should run hideInfo if 'closable' is in included the className", () => {
-
+      const mockEvent = { target: { className: "closable" }};
+      wrapper.instance().handleClick(mockEvent);
+      expect(wrapper.instance().props.hideInfo).toHaveBeenCalled();
     })
 
     it("should run toggleDetail if 'instruction-step' is included in the className", () => {
-
+      const mockEvent = { target: { className: "instruction-step" }};
+      wrapper.instance().toggleDetail = jest.fn();
+      wrapper.instance().handleClick(mockEvent);
+      expect(wrapper.instance().toggleDetail).toHaveBeenCalled();
     })
 
-    it("should run closeDetail if 'instruction-btep' is included in the className", () => {
-
+    it("should run closeDetail if 'instruction-box' is included in the className", () => {
+      const mockEvent = { target: { className: "instruction-box", parentElement: { parentElement: { className: "instruction-step project-step" }}}};
+      const selectedElement = jest.fn();
+      wrapper.instance().closeDetail = jest.fn();
+      wrapper.instance().handleClick(mockEvent);
+      expect(wrapper.instance().closeDetail).toHaveBeenCalled();
     })
   })
 
-  describe("toggleDetails", () => {
+  describe("toggleDetail", () => {
     it("should add 'show-info' to a class list if it is not there", () => {
 
+      let mockClassName = "instruction-step project-step";
+      document = wrapper;
+      expect(wrapper.find("#project-step").hasClass("show-info")).toEqual(false);
+      wrapper.instance().toggleDetail(mockClassName);
+      expect(wrapper.find("#project-step").hasClass("show-info")).toEqual(true);
     })
 
     it("should add 'arrow-down' to a class list if it is not there", () => {
-
+      let mockClassName = "instruction-step project-step";
+      expect(wrapper.find(".project-step").hasClass("arrow-down")).toEqual(false);
+      wrapper.instance().toggleDetail(mockClassName);
+      expect(wrapper.find(".project-step").hasClass("arrow-down")).toEqual(true);
     })
 
     it("should remove 'show-info' to a class list if it is there already", () => {
-
+      let mockClassName = "instruction-step project-step";
+      wrapper.instance().toggleDetail(mockClassName);
+      expect(wrapper.find("#project-step").hasClass("show-info")).toEqual(true);
+      wrapper.instance().toggleDetail(mockClassName);
+      expect(wrapper.find("#project-step").hasClass("show-info")).toEqual(false);
     })
 
     it("should remove 'arrow-down' to a class list if it is there already", () => {
-
+      let mockClassName = "instruction-step project-step";
+      wrapper.instance().toggleDetail(mockClassName);
+      expect(wrapper.find(".project-step").hasClass("arrow-down")).toEqual(true);
+      wrapper.instance().toggleDetail(mockClassName);
+      expect(wrapper.find(".project-step").hasClass("arrow-down")).toEqual(false);
     })
   })
 
-  describe("closeDetails", () => {
-    it("should remove 'show-info' to a class list if it is there already", () => {
-
+  describe("closeDetail", () => {
+    it("should remove 'show-info' from a detail", () => {
+      let mockClassName = "instruction-step project-step";
+      wrapper.instance().toggleDetail(mockClassName);
+      expect(wrapper.find("#project-step").hasClass("show-info")).toEqual(true);
+      wrapper.instance().closeDetail(mockClassName);
+      expect(wrapper.find("#project-step").hasClass("show-info")).toEqual(false);
     })
     
-    it("should remove 'arrow-down' to a class list if it is there already", () => {
-
+    it("should remove 'arrow-down' from a detail", () => {
+      let mockClassName = "instruction-step project-step";
+      wrapper.instance().toggleDetail(mockClassName);
+      expect(wrapper.find(".project-step").hasClass("arrow-down")).toEqual(true);
+      wrapper.instance().closeDetail(mockClassName);
+      expect(wrapper.find(".project-step").hasClass("arrow-down")).toEqual(false);
     })
   })
 })
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,10 @@
-import {configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import {configure} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
+
+// **** Script related to mock document: https://stackoverflow.com/questions/41098009/mocking-document-in-jest
+
+// Object.defineProperty(document, "currentScript", {
+//   value: document.createElement("script"),
+// });
